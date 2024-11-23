58f4cffbb7b69518df4439c5f5b8b7270d20c4f1
Arjun Singh Rajput <149608752+ArjunRajputGLA@users.noreply.github.com>
2024-11-23 21:34:12 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = "https://export-ease-api.vercel.app/auth/home";
+            const url = "https://export-ease-api.vercel.app/products";
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
